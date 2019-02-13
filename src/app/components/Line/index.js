9b0e2cd0ff68a960ba8b914576bf6fdbe9c4787e
Sebastian Balay <snbalay@gmail.com>
2019-02-13 18:33:06 +0100

    moving lines

--- src/app/components/Line/index.js
+++ src/app/components/Line/index.js
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "./styles.module.scss";
 
 export function Line({ red = false, rotation = 0, right = false }) {
+  const [moving, setMoving] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setMoving(true);
+    }, 100);
+    return () => clearTimeout(timeout);
+  }, []);
+
   const classes = `${styles.line} ${red ? styles.red : styles.blue} ${
     right ? styles.right : styles.left
   }`;
-  return <div className={classes} style={{ transform: `rotate(${rotation}deg)` }} />;
+
+  return (
+    <div
+      className={classes}
+      style={{ transform: `rotate(${rotation}deg)`, left: moving ? "200%" : 0 }}
+    />
+  );
 }
