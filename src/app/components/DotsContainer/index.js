979e67b37859642ce7518f6e096f8b2683dade81
Sebastian Balay <snbalay@gmail.com>
2019-02-13 20:50:50 +0100

    drag rotation hook

--- src/app/components/DotsContainer/index.js
+++ src/app/components/DotsContainer/index.js
@@ -1,31 +1,12 @@
-import React, { useState, useRef } from "react";
+import React from "react";
 
-import { calculateRotationAngle } from "../../utils/MathUtils";
-import { useDragHandler } from "../../hooks/useDragHandler";
+import { useRotationDragHandler } from "../../hooks/useRotationDragHandler";
 import { Dot } from "../Dot";
 
 import styles from "./styles.module.scss";
 
 export function DotsContainer() {
-  const [rotationDegress, setRotationDegress] = useState(0);
-  const lastAngleRef = useRef(null);
-
-  const [containerEl, dragging] = useDragHandler(handleMove, clearAngle);
-
-  function handleMove({ changedTouches, x, y }) {
-    const { clientX, clientY } = changedTouches ? changedTouches[0] : { clientX: x, clientY: y };
-    const rotationAngle = calculateRotationAngle(containerEl.current, { clientX, clientY });
-    const lastAngle = lastAngleRef.current;
-    lastAngleRef.current = rotationAngle;
-    if (!lastAngle) {
-      return;
-    }
-    setRotationDegress(before => before - lastAngle + rotationAngle);
-  }
-
-  function clearAngle() {
-    lastAngleRef.current = null;
-  }
+  const [containerEl, dragging, rotationDegress] = useRotationDragHandler();
 
   return (
     <div
---
+++ src/app/hooks/useRotationDragHandler.js
@@ -0,0 +1,42 @@
+import { useRef, useEffect, useState } from "react";
+
+import { calculateRotationAngle } from "../utils/MathUtils";
+
+export function useRotationDragHandler() {
+  const containerEl = useRef(null);
+  const lastAngleRef = useRef(null);
+  const [dragging, setDragging] = useState(false);
+  const [rotationDegress, setRotationDegress] = useState(false);
+
+  function handleMove({ changedTouches, x, y }) {
+    const { clientX, clientY } = changedTouches ? changedTouches[0] : { clientX: x, clientY: y };
+    const rotationAngle = calculateRotationAngle(containerEl.current, { clientX, clientY });
+    const lastAngle = lastAngleRef.current;
+    lastAngleRef.current = rotationAngle;
+    if (!lastAngle) {
+      return;
+    }
+    setRotationDegress(before => before - lastAngle + rotationAngle);
+  }
+
+  function stopRecording() {
+    containerEl.current.removeEventListener("touchmove", handleMove);
+    containerEl.current.removeEventListener("mousemove", handleMove);
+    setDragging(false);
+  }
+
+  function startRecording({ clientX, clientY }) {
+    lastAngleRef.current = null;
+    containerEl.current.addEventListener("touchmove", handleMove);
+    containerEl.current.addEventListener("mousemove", handleMove);
+    setDragging(true);
+  }
+
+  useEffect(() => {
+    containerEl.current.addEventListener("mouseup", stopRecording);
+    containerEl.current.addEventListener("mousedown", startRecording);
+    // TODO: remove listeners
+  }, []);
+
+  return [containerEl, dragging, rotationDegress];
+}
